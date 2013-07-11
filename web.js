0e8ad33ad4317cf5ef3d66356864e2ff805038d8
hindu-local <system@ubuntu.(none)>
2013-07-10 23:49:38 -0700

    bug

--- web.js
+++ web.js
@@ -3,14 +3,12 @@ var fs = require('fs');
 
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-
-
-  response.send(fs.readFileSync('index.html'));
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
