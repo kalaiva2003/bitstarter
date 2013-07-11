ea75cbe856234989935d686da205e7a2ba71113a
hindu-local <system@ubuntu.(none)>
2013-07-10 23:29:24 -0700

    index.html displayed in app

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = required('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+
+  response.send(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
